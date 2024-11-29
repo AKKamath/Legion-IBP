d94301850e9cce59d18a2f66708cfa4f86e019ea
Aditya Kamath <akkamath@uw.edu>
2024-11-29 16:09:07 +0000

    Add slurm scripts

---
+++ scripts/sbatch/sbatch_expts.sh
@@ -0,0 +1,22 @@
+#!/bin/bash --login
+#
+#SBATCH --job-name=gnn_run
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+#
+#SBATCH --time=10:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=300GB
+#SBATCH --gres=gpu:a100:1
+
+set -e # stop bash script on first error
+
+conda activate dgl-dev-gpu-117
+
+export CUDA_MPS_PIPE_DIRECTORY=~/Legion/mps-a100 # Select a location that's accessible to the given $UID
+export CUDA_MPS_LOG_DIRECTORY=~/Legion/mps-a100-log # Select a location that's accessible to the given $UID
+nvidia-cuda-mps-control -d # Start the daemon.
+
+# Optionally add "$@" to pass additional arguments to the script
+make run_expts_singlegpu RESULTS=./results_a100
---
+++ scripts/sbatch/srun.sh
@@ -0,0 +1,1 @@
+srun --time=10:00:00 --mem=250GB --gres=gpu:a100:1 --pty bash -i
