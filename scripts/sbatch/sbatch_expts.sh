aa2b374f86c0123abdac0e52a346559827995e53
Aditya Kamath <akkamath@uw.edu>
2026-02-10 00:05:42 +0000

    Save sbatch stuff

--- scripts/sbatch/sbatch_expts.sh
+++ scripts/sbatch/sbatch_expts.sh
@@ -4,19 +4,28 @@
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.err
 #
-#SBATCH --time=10:00:00
+#SBATCH --time=14:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=300GB
 #SBATCH --gres=gpu:a100:1
+#SBATCH --nodelist=mcnode22
 
-set -e # stop bash script on first error
+#set -e # stop bash script on first error
 
-conda activate dgl-dev-gpu-117
+#conda activate dgl-dev-gpu-117
 
 export CUDA_MPS_PIPE_DIRECTORY=~/Legion/mps-a100 # Select a location that's accessible to the given $UID
 export CUDA_MPS_LOG_DIRECTORY=~/Legion/mps-a100-log # Select a location that's accessible to the given $UID
 nvidia-cuda-mps-control -d # Start the daemon.
 
+bash build.sh
+
+pushd sampling_server/src/IBP/
+rm -rf build/
+pip install -v -e .
+popd
+
 # Optionally add "$@" to pass additional arguments to the script
-make run_expts_singlegpu RESULTS=./results_a100
+make run_mag_all_singlegpu RESULTS=./results_new3_a100
+#make run_comptests RESULTS=./results_new3_a100
--- scripts/sbatch/srun.sh
+++ scripts/sbatch/srun.sh
@@ -1,1 +1,1 @@
-srun --time=10:00:00 --mem=250GB --gres=gpu:a100:1 --pty bash -i
+srun --time=4:00:00 --mem=300GB --gres=gpu:a100:1 --nodelist=mcnode22 --pty bash -i
