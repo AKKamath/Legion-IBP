0b0695fd470695bc6326ac0d2d9d117d422089c5
Aditya Kamath <akkamath@uw.edu>
2026-05-25 00:37:37 +0000

    Add 32 CPU to sbatch script

--- scripts/sbatch/srun.sh
+++ scripts/sbatch/srun.sh
@@ -1,1 +1,1 @@
-srun --time=4:00:00 --mem=300GB --gres=gpu:a100:1 --nodelist=mcnode22 --pty bash -i
+srun --time=4:00:00 --mem=300GB --gres=gpu:a100:1 --cpus-per-task=32 --nodelist=mcnode22 --pty bash -i
